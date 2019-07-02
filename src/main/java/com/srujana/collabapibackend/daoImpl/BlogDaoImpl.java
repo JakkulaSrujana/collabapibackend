27a11ac47dc370f0e441d8474bcdca45e0b7c131
SRUJANA <SRUJANA@DESKTOP-2Q9LFBI>
2019-07-02 14:07:10 +0530

    commit

--- src/main/java/com/srujana/collabapibackend/daoImpl/BlogDaoImpl.java
+++ src/main/java/com/srujana/collabapibackend/daoImpl/BlogDaoImpl.java
@@ -2,15 +2,31 @@ package com.srujana.collabapibackend.daoImpl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.srujana.collabapibackend.dao.blogDao;
 import com.srujana.collabapibackend.model.Blog;
 
+@Component("blogDaoImpl")
 public class BlogDaoImpl implements blogDao {
 
+	@Autowired
+	SessionFactory sessionFactory;
+	
 	@Override
 	public void addBlog(Blog blog) {
-		// TODO Auto-generated method stub
 		
+		System.out.println(sessionFactory);
+		Session session=sessionFactory.openSession();
+		session.save(blog);
+		Transaction transaction=session.beginTransaction();
+		transaction.commit();	
+		session.close();	
+			
 	}
 
 	@Override
--- src/main/java/com/srujana/collabapibackend/model/Blog.java
+++ src/main/java/com/srujana/collabapibackend/model/Blog.java
@@ -1,10 +1,25 @@
 package com.srujana.collabapibackend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Blog {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int blogId;
 	private String blogName;
 	private String blogTitle;
 	private String blogOwner;
+	public int getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(int blogId) {
+		this.blogId = blogId;
+	}
 	public String getBlogName() {
 		return blogName;
 	}
